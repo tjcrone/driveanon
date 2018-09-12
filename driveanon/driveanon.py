a86e3308c32ba2f2ad33439e797668c50da20d98
Tim Crone <tjcrone@gmail.com>
2018-09-12 18:13:42 -0400

    Start working on the blob lister

--- driveanon/driveanon.py
+++ driveanon/driveanon.py
@@ -10,12 +10,19 @@ def _get_token(response):
 def open_file(blob_id):
     """ Read a file from Google Drive into memory. Returns an open (BytesIO) file-like object. """
     
-    url = "https://docs.google.com/uc?export=download"
+    url = 'https://docs.google.com/uc?export=download'
     session = requests.Session()
-    response = session.get(url, params = { 'id' : blob_id }, stream = True,)
+    response = session.get(url, params = { 'id' : blob_id }, stream = True)
     token = _get_token(response)
     if token:
         params = { 'id' : blob_id, 'confirm' : token }
         response = session.get(url, params = params, stream = True)
     file_bytes = response.content
     return io.BytesIO(file_bytes)
+
+def list_blobs(blob_id):
+    blob_id = '1mn2Q1Gm0WEI51G_1A6SNsDJqV7jWVC-f'
+    url = "https://drive.google.com/drive/folders/%s" % blob_id
+    session = requests.Session()
+    response = session.get(url, params = { 'usp' : 'sharing' })
+    return response
